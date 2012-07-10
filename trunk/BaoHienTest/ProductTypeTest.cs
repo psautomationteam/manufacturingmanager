41b166ad4ee655b14ed99eafd56a5a275a3f66ad
stephentran84@gmail.com <stephentran84@gmail.com@9ed1cdc3-d50c-7e01-3ac5-32208a8fc879>
2012-07-10 19:01:14 +0000

    add Product Service

---
+++ trunk/BaoHienTest/ProductTypeTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAL;
+using BaoHienServices.Helper;
+using BaoHienServices;
+
+namespace BaoHienTest
+{
+    [TestClass]
+    public class ProductTypeTest
+    {
+        [TestMethod]
+        public void AddProductType()
+        {
+            //List<ProductType> list = (List<ProductType>)BaoHienRepository.SelectAll<ProductType>().Where<ProductType>(item => item.st == false).ToList<ProductType>();
+            //int count = list.Count;
+            ProductType productType = new ProductType
+            {
+                Description = "P Type 1",
+                ProductName = "Product 1",
+                TypeCode = "Type code 1"
+            };
+            ProductTypeService productTypeService = new ProductTypeService();
+            bool result = productTypeService.AddProductType(productType);
+
+            Assert.AreEqual<bool>(true, result); ;
+        }
+        [TestMethod]
+        public void GetProductType()
+        {
+            //List<ProductType> list = (List<ProductType>)BaoHienRepository.SelectAll<ProductType>().Where<ProductType>(item => item.st == false).ToList<ProductType>();
+            //int count = list.Count;
+           
+            ProductTypeService productTypeService = new ProductTypeService();
+            ProductType productType = productTypeService.GetProductType(4);
+
+            Assert.AreEqual<int>(4, productType.Id); ;
+        }
+        [TestMethod]
+        public void GetProductTypes()
+        {
+            //List<ProductType> list = (List<ProductType>)BaoHienRepository.SelectAll<ProductType>().Where<ProductType>(item => item.st == false).ToList<ProductType>();
+            //int count = list.Count;
+
+            ProductTypeService productTypeService = new ProductTypeService();
+            List<ProductType> productTypes = productTypeService.GetProductTypes();
+
+            Assert.AreEqual<bool>(true, productTypes.Count > 0); ;
+        }
+        
+    }
+}
