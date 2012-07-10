4b8ef621055772b0dc9d6ef3d61101f3cc3873d8
duchungpham12d2 <duchungpham12d2@gmail.com>
2012-07-10 17:26:09 +0000

    Update default namespace from UI to BaoHien.UI Add login form

--- UI/Program.cs
+++ UI/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using UI;
+using BaoHien.UI;
 namespace Com.Baohien
 {
     static class Program
@@ -15,7 +15,7 @@ namespace Com.Baohien
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
--- UI/UI/Main.Designer.cs
+++ UI/UI/Main.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace UI
+﻿namespace BaoHien.UI
 {
     partial class Main
     {
