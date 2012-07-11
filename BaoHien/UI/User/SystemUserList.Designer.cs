afed7edec016565833bd490efafd5462eaecaa06
duchungpham12d2 <duchungpham12d2@gmail.com>
2012-07-11 16:40:50 +0000

    Add form for user and employee

---
+++ BaoHien/UI/User/SystemUserList.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace BaoHien.UI
+{
+    partial class SystemUserList
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvUserList = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUserList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvUserList
+            // 
+            this.dgvUserList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUserList.Location = new System.Drawing.Point(47, 71);
+            this.dgvUserList.Name = "dgvUserList";
+            this.dgvUserList.Size = new System.Drawing.Size(787, 469);
+            this.dgvUserList.TabIndex = 17;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(169, 22);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(107, 43);
+            this.btnAdd.TabIndex = 16;
+            this.btnAdd.Text = "Thêm người dùng mới";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(49, 22);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(104, 43);
+            this.btnDelete.TabIndex = 18;
+            this.btnDelete.Text = "Tạm dừng người được chọn";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // SystemUserList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dgvUserList);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDelete);
+            this.Name = "SystemUserList";
+            this.Size = new System.Drawing.Size(1084, 641);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUserList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvUserList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
