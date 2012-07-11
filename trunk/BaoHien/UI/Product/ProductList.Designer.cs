30ca64d7579e61f13e6b90ac2999f77423cdd96f
stephentran84@gmail.com <stephentran84@gmail.com@9ed1cdc3-d50c-7e01-3ac5-32208a8fc879>
2012-07-11 16:32:20 +0000

    load data to form

--- trunk/BaoHien/UI/Product/ProductList.Designer.cs
+++ trunk/BaoHien/UI/Product/ProductList.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.grpSearch = new System.Windows.Forms.GroupBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbProductTypes = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.btnSearch = new System.Windows.Forms.Button();
@@ -50,7 +50,7 @@
             // grpSearch
             // 
             this.grpSearch.Controls.Add(this.comboBox2);
-            this.grpSearch.Controls.Add(this.comboBox1);
+            this.grpSearch.Controls.Add(this.cbProductTypes);
             this.grpSearch.Controls.Add(this.label4);
             this.grpSearch.Controls.Add(this.label5);
             this.grpSearch.Controls.Add(this.btnSearch);
@@ -58,9 +58,9 @@
             this.grpSearch.Controls.Add(this.txtCode);
             this.grpSearch.Controls.Add(this.label2);
             this.grpSearch.Controls.Add(this.label1);
-            this.grpSearch.Location = new System.Drawing.Point(28, 14);
+            this.grpSearch.Location = new System.Drawing.Point(28, 15);
             this.grpSearch.Name = "grpSearch";
-            this.grpSearch.Size = new System.Drawing.Size(787, 99);
+            this.grpSearch.Size = new System.Drawing.Size(787, 107);
             this.grpSearch.TabIndex = 6;
             this.grpSearch.TabStop = false;
             this.grpSearch.Text = "Tìm Kiếm";
@@ -71,56 +71,56 @@
             this.comboBox2.Items.AddRange(new object[] {
             "Còn giao dịch",
             "Hết giao dịch"});
-            this.comboBox2.Location = new System.Drawing.Point(448, 56);
+            this.comboBox2.Location = new System.Drawing.Point(448, 60);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(170, 21);
+            this.comboBox2.Size = new System.Drawing.Size(170, 22);
             this.comboBox2.TabIndex = 8;
             // 
-            // comboBox1
+            // cbProductTypes
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(147, 57);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(169, 21);
-            this.comboBox1.TabIndex = 7;
+            this.cbProductTypes.FormattingEnabled = true;
+            this.cbProductTypes.Location = new System.Drawing.Point(147, 61);
+            this.cbProductTypes.Name = "cbProductTypes";
+            this.cbProductTypes.Size = new System.Drawing.Size(169, 22);
+            this.cbProductTypes.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(368, 62);
+            this.label4.Location = new System.Drawing.Point(368, 67);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.Size = new System.Drawing.Size(61, 14);
             this.label4.TabIndex = 6;
             this.label4.Text = "Trạng Thái:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(42, 62);
+            this.label5.Location = new System.Drawing.Point(42, 67);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.Size = new System.Drawing.Size(80, 14);
             this.label5.TabIndex = 5;
             this.label5.Text = "Loại sản phẩm:";
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(661, 28);
+            this.btnSearch.Location = new System.Drawing.Point(661, 30);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(75, 54);
+            this.btnSearch.Size = new System.Drawing.Size(75, 58);
             this.btnSearch.TabIndex = 4;
             this.btnSearch.Text = "Tìm";
             this.btnSearch.UseVisualStyleBackColor = true;
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(448, 28);
+            this.txtName.Location = new System.Drawing.Point(448, 30);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(170, 20);
             this.txtName.TabIndex = 3;
             // 
             // txtCode
             // 
-            this.txtCode.Location = new System.Drawing.Point(146, 28);
+            this.txtCode.Location = new System.Drawing.Point(146, 30);
             this.txtCode.Name = "txtCode";
             this.txtCode.Size = new System.Drawing.Size(170, 20);
             this.txtCode.TabIndex = 1;
@@ -128,35 +128,35 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(352, 31);
+            this.label2.Location = new System.Drawing.Point(352, 33);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.Size = new System.Drawing.Size(78, 14);
             this.label2.TabIndex = 2;
             this.label2.Text = "Tên sản phẩm:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 31);
+            this.label1.Location = new System.Drawing.Point(47, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.Size = new System.Drawing.Size(74, 14);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mã sản phẩm:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(28, 131);
+            this.label3.Location = new System.Drawing.Point(28, 141);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(132, 13);
+            this.label3.Size = new System.Drawing.Size(132, 14);
             this.label3.TabIndex = 10;
             this.label3.Text = "Tổng số kết quả tìm được:";
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(644, 119);
+            this.btnDelete.Location = new System.Drawing.Point(644, 128);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(75, 43);
+            this.btnDelete.Size = new System.Drawing.Size(75, 46);
             this.btnDelete.TabIndex = 9;
             this.btnDelete.Text = "Xóa loại được chọn";
             this.btnDelete.UseVisualStyleBackColor = true;
@@ -164,16 +164,16 @@
             // dgvProductList
             // 
             this.dgvProductList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvProductList.Location = new System.Drawing.Point(28, 172);
+            this.dgvProductList.Location = new System.Drawing.Point(28, 185);
             this.dgvProductList.Name = "dgvProductList";
-            this.dgvProductList.Size = new System.Drawing.Size(787, 469);
+            this.dgvProductList.Size = new System.Drawing.Size(787, 505);
             this.dgvProductList.TabIndex = 8;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(740, 119);
+            this.btnAdd.Location = new System.Drawing.Point(740, 128);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(75, 43);
+            this.btnAdd.Size = new System.Drawing.Size(75, 46);
             this.btnAdd.TabIndex = 7;
             this.btnAdd.Text = "Thêm sản phẩm";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -182,17 +182,17 @@
             // lblTotalResult
             // 
             this.lblTotalResult.AutoSize = true;
-            this.lblTotalResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalResult.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTotalResult.ForeColor = System.Drawing.Color.Blue;
-            this.lblTotalResult.Location = new System.Drawing.Point(164, 126);
+            this.lblTotalResult.Location = new System.Drawing.Point(164, 140);
             this.lblTotalResult.Name = "lblTotalResult";
-            this.lblTotalResult.Size = new System.Drawing.Size(26, 17);
+            this.lblTotalResult.Size = new System.Drawing.Size(24, 16);
             this.lblTotalResult.TabIndex = 11;
             this.lblTotalResult.Text = "10";
             // 
             // ProductList
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.grpSearch);
             this.Controls.Add(this.label3);
@@ -201,7 +201,8 @@
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.lblTotalResult);
             this.Name = "ProductList";
-            this.Size = new System.Drawing.Size(1084, 641);
+            this.Size = new System.Drawing.Size(1084, 690);
+            this.Load += new System.EventHandler(this.ProductList_Load);
             this.grpSearch.ResumeLayout(false);
             this.grpSearch.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductList)).EndInit();
@@ -223,7 +224,7 @@
         private System.Windows.Forms.DataGridView dgvProductList;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Label lblTotalResult;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbProductTypes;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ComboBox comboBox2;
