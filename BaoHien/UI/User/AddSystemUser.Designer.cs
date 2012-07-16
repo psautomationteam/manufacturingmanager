a9c1bd2d4c1eb506c8c0256cd4eb56a586b115a8
duchungpham12d2 <duchungpham12d2@gmail.com>
2012-07-16 16:54:54 +0000

    Add confirm passowd field

--- BaoHien/UI/User/AddSystemUser.Designer.cs
+++ BaoHien/UI/User/AddSystemUser.Designer.cs
@@ -39,6 +39,8 @@
             this.label4 = new System.Windows.Forms.Label();
             this.cbUserType = new System.Windows.Forms.ComboBox();
             this.lblErrorMessage = new System.Windows.Forms.Label();
+            this.txtConfirmPass = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label5
@@ -46,7 +48,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Blue;
-            this.label5.Location = new System.Drawing.Point(2, 178);
+            this.label5.Location = new System.Drawing.Point(2, 207);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(210, 14);
             this.label5.TabIndex = 17;
@@ -54,9 +56,9 @@
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(331, 156);
+            this.btnAdd.Location = new System.Drawing.Point(331, 187);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(99, 25);
+            this.btnAdd.Size = new System.Drawing.Size(99, 23);
             this.btnAdd.TabIndex = 16;
             this.btnAdd.Text = "Thêm";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -64,14 +66,14 @@
             // 
             // txtCode
             // 
-            this.txtCode.Location = new System.Drawing.Point(152, 53);
+            this.txtCode.Location = new System.Drawing.Point(152, 49);
             this.txtCode.Name = "txtCode";
             this.txtCode.Size = new System.Drawing.Size(178, 20);
             this.txtCode.TabIndex = 14;
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(152, 17);
+            this.txtName.Location = new System.Drawing.Point(152, 16);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(278, 20);
             this.txtName.TabIndex = 13;
@@ -79,43 +81,44 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(61, 87);
+            this.label3.Location = new System.Drawing.Point(61, 81);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 14);
+            this.label3.Size = new System.Drawing.Size(68, 13);
             this.label3.TabIndex = 11;
             this.label3.Text = "Mật khẩu (*):";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 53);
+            this.label2.Location = new System.Drawing.Point(32, 49);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(97, 14);
+            this.label2.Size = new System.Drawing.Size(97, 13);
             this.label2.TabIndex = 10;
             this.label2.Text = "Tên đăng nhập (*):";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(70, 17);
+            this.label1.Location = new System.Drawing.Point(70, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 14);
+            this.label1.Size = new System.Drawing.Size(59, 13);
             this.label1.TabIndex = 9;
             this.label1.Text = "Tên gọi (*):";
             // 
             // txtPass
             // 
-            this.txtPass.Location = new System.Drawing.Point(152, 87);
+            this.txtPass.Location = new System.Drawing.Point(152, 81);
             this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
             this.txtPass.Size = new System.Drawing.Size(178, 20);
             this.txtPass.TabIndex = 18;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(43, 117);
+            this.label4.Location = new System.Drawing.Point(43, 151);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(88, 14);
+            this.label4.Size = new System.Drawing.Size(86, 13);
             this.label4.TabIndex = 19;
             this.label4.Text = "Lọai người dùng:";
             // 
@@ -126,24 +129,44 @@
             "Kế Toán",
             "Quản Trị",
             "Người Bán Hàng"});
-            this.cbUserType.Location = new System.Drawing.Point(152, 117);
+            this.cbUserType.Location = new System.Drawing.Point(152, 151);
             this.cbUserType.Name = "cbUserType";
-            this.cbUserType.Size = new System.Drawing.Size(121, 22);
+            this.cbUserType.Size = new System.Drawing.Size(121, 21);
             this.cbUserType.TabIndex = 20;
             // 
             // lblErrorMessage
             // 
             this.lblErrorMessage.AutoSize = true;
-            this.lblErrorMessage.Location = new System.Drawing.Point(22, 156);
+            this.lblErrorMessage.Location = new System.Drawing.Point(22, 145);
             this.lblErrorMessage.Name = "lblErrorMessage";
-            this.lblErrorMessage.Size = new System.Drawing.Size(0, 14);
+            this.lblErrorMessage.Size = new System.Drawing.Size(0, 13);
             this.lblErrorMessage.TabIndex = 21;
+            // 
+            // txtConfirmPass
+            // 
+            this.txtConfirmPass.Location = new System.Drawing.Point(152, 116);
+            this.txtConfirmPass.Name = "txtConfirmPass";
+            this.txtConfirmPass.PasswordChar = '*';
+            this.txtConfirmPass.Size = new System.Drawing.Size(178, 20);
+            this.txtConfirmPass.TabIndex = 23;
+            this.txtConfirmPass.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Xác nhận mật khẩu (*):";
             // 
             // AddSystemUser
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 201);
+            this.ClientSize = new System.Drawing.Size(455, 234);
+            this.Controls.Add(this.txtConfirmPass);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.lblErrorMessage);
             this.Controls.Add(this.cbUserType);
             this.Controls.Add(this.label4);
@@ -175,5 +198,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox cbUserType;
         private System.Windows.Forms.Label lblErrorMessage;
+        private System.Windows.Forms.TextBox txtConfirmPass;
+        private System.Windows.Forms.Label label6;
     }
 }
