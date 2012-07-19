1f3ee51fa1cdc5521dc828d4f60dd712c5f61fc3
stephentran84@gmail.com <stephentran84@gmail.com@9ed1cdc3-d50c-7e01-3ac5-32208a8fc879>
2012-07-19 15:56:08 +0000

    order

---
+++ trunk/BaoHien/Common/Global.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BaoHien.UI;
+using DAL;
+
+namespace BaoHien.Common
+{
+    public class Global
+    {
+        static SystemUser currentUser = null;
+        public static SystemUser CurrentUser
+        {
+            get
+            {
+                return currentUser;
+            }
+            set
+            {
+                currentUser = value;
+            }
+        }
+    }
+}
