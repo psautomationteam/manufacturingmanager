9f426a66cec75ee3540abf703a7b2065b1a96b88
duchungpham12d2@gmail.com <duchungpham12d2@gmail.com@9ed1cdc3-d50c-7e01-3ac5-32208a8fc879>
2012-08-02 19:07:41 +0000

    minor update

--- trunk/BaoHien/UI/Billing/AddBill.Designer.cs
+++ trunk/BaoHien/UI/Billing/AddBill.Designer.cs
@@ -143,6 +143,7 @@
             this.btnCancel.TabIndex = 24;
             this.btnCancel.Text = "Hủy";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // AddBill
             // 
