79efe1476e127e906cf3849fb18d8c02c6a2cb0b
stephentran84 <stephentran84@gmail.com>
2012-08-15 18:57:37 +0000

    add printer

--- BaoHien/UI/Order/AddOrder.cs
+++ BaoHien/UI/Order/AddOrder.cs
@@ -17,7 +17,10 @@ using BaoHien.Services.OrderDetails;
 using BaoHien.Services.BaseAttributes;
 using BaoHien.Services.ProductAttributes;
 using BaoHien.Model;
-
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.IO;
+using CoolPrintPreview;
 namespace BaoHien.UI
 {
     public partial class AddOrder : BaseForm
@@ -28,6 +31,34 @@ namespace BaoHien.UI
         BindingList<OrderDetail> orderDetails;
         BindingList<Product> products;
         BindingList<BaseAttribute> baseAttributesAtRow;
+
+        private System.IO.Stream streamToPrint;
+        Image MyImage;
+        string streamType;
+        [System.Runtime.InteropServices.DllImportAttribute("gdi32.dll")]
+        private static extern bool BitBlt
+
+        (
+
+            IntPtr hdcDest, // handle to destination DC
+
+            int nXDest, // x-coord of destination upper-left corner
+
+            int nYDest, // y-coord of destination upper-left corner
+
+            int nWidth, // width of destination rectangle
+
+            int nHeight, // height of destination rectangle
+
+            IntPtr hdcSrc, // handle to source DC
+
+            int nXSrc, // x-coordinate of source upper-left corner
+
+            int nYSrc, // y-coordinate of source upper-left corner
+
+            System.Int32 dwRop // raster operation code
+
+        );
         public AddOrder()
         {
             InitializeComponent();
@@ -565,6 +596,115 @@ namespace BaoHien.UI
         {
             
         }
+
+        private void CaptureScreen()
+        {
+            Graphics g1 = this.CreateGraphics();
+
+            MyImage = new Bitmap(this.ClientRectangle.Width, this.ClientRectangle.Height, g1);
+
+            Graphics g2 = Graphics.FromImage(MyImage);
+
+            IntPtr dc1 = g1.GetHdc();
+
+            IntPtr dc2 = g2.GetHdc();
+
+            BitBlt(dc2, 0, 0, this.ClientRectangle.Width, this.ClientRectangle.Height, dc1, 0, 0, 13369376);
+
+            g1.ReleaseHdc(dc1);
+
+            g2.ReleaseHdc(dc2);
+
+            //MyImage.Save(@"c:\PrintPage.jpg", ImageFormat.Jpeg);
+        }
+        private void btnPrintOrder_Click(object sender, EventArgs e)
+        {
+
+            CaptureScreen();
+            using (var dlg = new CoolPrintPreviewDialog())
+            {
+                dlg.Document = this.printDoc;
+                dlg.ShowDialog(this);
+            }
+           
+            //FileStream fileStream = new FileStream(@"c:\PrintPage.jpg", FileMode.Open, FileAccess.Read);
+
+            //StartPrint(fileStream, "Image");
+
+            //fileStream.Close();
+
+            //if (System.IO.File.Exists(@"c:\PrintPage.jpg"))
+            //{
+
+            //    System.IO.File.Delete(@"c:\PrintPage.jpg");
+
+            //}
+        }
+        public void StartPrint(Stream streamToPrint, string streamType)
+        {
+
+            this.printDoc.PrintPage += new PrintPageEventHandler(printDoc_PrintPage);
+
+            this.streamToPrint = streamToPrint;
+
+            this.streamType = streamType;
+
+            System.Windows.Forms.PrintDialog PrintDialog1 = new PrintDialog();
+
+            PrintDialog1.AllowSomePages = true;
+
+            PrintDialog1.ShowHelp = true;
+
+            PrintDialog1.Document = printDoc;
+
+            DialogResult result = PrintDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+
+                printDoc.Print();
+
+                //docToPrint.Print();
+
+            }
+        }
+        private void printDoc_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            //System.Drawing.Image image = System.Drawing.Image.FromStream(this.streamToPrint);
+            //;
+            //int x = e.MarginBounds.X;
+
+            //int y = e.MarginBounds.Y;
+
+            //int width = image.Width;
+
+            //int height = image.Height;
+
+            //if ((width / e.MarginBounds.Width) > (height / e.MarginBounds.Height))
+            //{
+
+            //    width = e.MarginBounds.Width;
+
+            //    height = image.Height * e.MarginBounds.Width / image.Width;
+
+            //}
+
+            //else
+            //{
+
+            //    height = e.MarginBounds.Height;
+
+            //    width = image.Width * e.MarginBounds.Height / image.Height;
+
+            //}
+
+            //System.Drawing.Rectangle destRect = new System.Drawing.Rectangle(x, y, width, height);
+
+            //e.Graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, System.Drawing.GraphicsUnit.Pixel);
+            e.Graphics.DrawImage(MyImage, 0, 0);
+            MyImage.Save(@"c:\PrintPage.jpg", ImageFormat.Jpeg);
+        }
+    
 
         
     }
