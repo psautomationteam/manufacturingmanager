db9a2a6252fa01e0b0f6760c0da8109985704223
stephentran84 <stephentran84@gmail.com>
2012-08-26 14:42:49 +0000

    add more print for XK

---
+++ BaoHien/Model/ArrearReportModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BaoHien.Model
+{
+    public class ArrearReportModel
+    {
+        string date;
+
+        public string Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+        string attributeName;
+
+        public string AttributeName
+        {
+            get { return attributeName; }
+            set { attributeName = value; }
+        }
+        string baseUnitName;
+
+        public string BaseUnitName
+        {
+            get { return baseUnitName; }
+            set { baseUnitName = value; }
+        }
+        int numberOfItem;
+
+        public int NumberOfItem
+        {
+            get { return numberOfItem; }
+            set { numberOfItem = value; }
+        }
+        double price;
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        double total;
+
+        public double Total
+        {
+            get { return total; }
+            set { total = value; }
+        }
+    }
+}
---
+++ BaoHien/UI/Report/ArrearReport.designer.cs
@@ -0,0 +1,147 @@
+﻿namespace BaoHien.UI
+{
+    partial class ArrearReport
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cbmCustomers = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgwStockEntranceList = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwStockEntranceList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.cbmCustomers);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.dtpFrom);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(19, 17);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(696, 68);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin tạo báo cáo";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(597, 20);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 38);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Báo Cáo";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // cbmCustomers
+            // 
+            this.cbmCustomers.FormattingEnabled = true;
+            this.cbmCustomers.Location = new System.Drawing.Point(383, 20);
+            this.cbmCustomers.Name = "cbmCustomers";
+            this.cbmCustomers.Size = new System.Drawing.Size(166, 22);
+            this.cbmCustomers.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(298, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 14);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Khách hàng:";
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFrom.Location = new System.Drawing.Point(116, 23);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(124, 20);
+            this.dtpFrom.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tại thời điểm:";
+            // 
+            // dgwStockEntranceList
+            // 
+            this.dgwStockEntranceList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwStockEntranceList.Location = new System.Drawing.Point(19, 129);
+            this.dgwStockEntranceList.Name = "dgwStockEntranceList";
+            this.dgwStockEntranceList.Size = new System.Drawing.Size(1053, 540);
+            this.dgwStockEntranceList.TabIndex = 21;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(16, 99);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(324, 14);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Chú ý: mặc định sẽ là tình trạng hiện tại trong kho và thành phẩm";
+            // 
+            // ArrearReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgwStockEntranceList);
+            this.Controls.Add(this.label8);
+            this.Name = "ArrearReport";
+            this.Size = new System.Drawing.Size(1084, 690);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwStockEntranceList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cbmCustomers;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgwStockEntranceList;
+        private System.Windows.Forms.Label label8;
+    }
+}
