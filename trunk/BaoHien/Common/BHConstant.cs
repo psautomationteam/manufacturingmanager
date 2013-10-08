650ae9b7273dfdfb117f4b1c39ffdcc6e1af4278
nguyenthinhkha <nguyenthinhkha@9ed1cdc3-d50c-7e01-3ac5-32208a8fc879>
2013-10-08 14:37:38 +0000

    Update #21

--- trunk/BaoHien/Common/BHConstant.cs
+++ trunk/BaoHien/Common/BHConstant.cs
@@ -7,8 +7,8 @@ namespace BaoHien.Common
 {
     public class BHConstant
     {
-        public const string BUILD_VERSION = "1.1.6";
-        public const string BUILD_RELEASE_DATE = "09/18/2013 21:00:00 AM";
+        public const string BUILD_VERSION = "1.2.0";
+        public const string BUILD_RELEASE_DATE = "10/08/2013 21:30:00 PM";
 
         public const string MASTER_USERNAME = "baohien";
         public const string MASTER_PASSWORD_TO_DELETE_ALL = "baohien123456";
